00a818d9ad5da4d5d1ff05670bcbb19aa1aa6a74
Rubysdad <markandtabithamorales@gmail.com>
2017-07-27 18:12:55 +0000

    SENDGRID AND CREATE CONTACT FORM BG PIC

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,16 +84,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://jvaz.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'jvaz.herokuapp.com', :protocol => 'https' }
   
-  ActionMailer::Base.smtp_settings = {
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD'],
-    :domain => 'heroku.com',
-    :address => 'smtp.sendgrid.net',
-    :port => 587,
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
+  
 end
