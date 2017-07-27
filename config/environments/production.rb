087a3a82acba1758bd7217fab249966ff71daf6b
Rubysdad <markandtabithamorales@gmail.com>
2017-07-27 16:49:30 +0000

    smtp bug fix and added container to new contact form

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,13 +84,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.default_url_options = { host: 'https://www.valentineazproperties.com' }
+  config.action_mailer.default_url_options = { host: 'https://valentineazproperties.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   
   ActionMailer::Base.smtp_settings = {
   :user_name => ENV['SENDGRID_USERNAME'],
   :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'valentineazproperties.com',
+  :domain => 'heroku.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
   :authentication => :plain,
