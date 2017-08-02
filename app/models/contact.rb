c897c865c8205fe33e3ef5019faa537bcc020126
Rubysdad <markandtabithamorales@gmail.com>
2017-08-02 22:30:30 +0000

    removed stellar js

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -6,7 +6,7 @@ class Contact < MailForm::Base
   
   def headers 
     {
-      :subject => "Contact Form",
+      :subject => "New Lead from Website!",
       :to => "julia.valentine@azmoves.com",
       :from => %("#{name}" <#{email}>)
     }
